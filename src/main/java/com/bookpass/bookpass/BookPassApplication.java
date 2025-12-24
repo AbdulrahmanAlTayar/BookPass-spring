1a705e21e4d946c7b30f0ae7baf2c6c850095391
AbdulrahmanAlTayar <10934@holbertonstudents.com>
2025-12-24 20:35:08 +0300

    v1 - Setup the project

---
+++ src/main/java/com/bookpass/bookpass/BookPassApplication.java
@@ -0,0 +1,13 @@
+package com.bookpass.bookpass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookPassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookPassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bookpass/bookpass/BookPassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookpass.bookpass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookPassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
