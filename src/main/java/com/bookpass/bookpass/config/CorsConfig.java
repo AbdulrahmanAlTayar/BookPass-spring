761aca31156f956ff6ac060a4673427d6686060f
abo-ah-md <abo-ah-md1@hotmail.com>
2026-01-13 00:49:16 +0300

    added cors fix

--- src/main/java/com/bookpass/bookpass/config/CorsConfig.java
+++ src/main/java/com/bookpass/bookpass/config/CorsConfig.java
@@ -26,11 +26,9 @@ public class CorsConfig {
             "http://127.0.0.1:5174",
             "http://127.0.0.1:5175",
             "http://127.0.0.1:3000",
-            "http://api.book-pass.com",
+            "https://api.book-pass.com",
             "https://book-pass.com",
-            "https://www.book-pass.com",
-           
-            
+            "https://www.book-pass.com"
         ));
 
         // Allow all common HTTP methods
