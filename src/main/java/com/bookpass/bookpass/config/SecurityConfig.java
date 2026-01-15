5a722c14ad4f8e179dbfd5dcd48c7e03672cd3ef
abo-ah-md <abo-ah-md1@hotmail.com>
2026-01-15 13:35:32 +0300

    added dashboard end point

--- src/main/java/com/bookpass/bookpass/config/SecurityConfig.java
+++ src/main/java/com/bookpass/bookpass/config/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/api/files/**").permitAll()
                         // Swagger UI & OpenAPI
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+                        .requestMatchers("/actuator/health").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
